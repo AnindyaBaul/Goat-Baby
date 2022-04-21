7ca362d9af0bfe67d2eec1664eca0a58dd61678c
Anindya <banindya99@gmail.com>
2022-04-22 03:27:18 +0600

    Pc version

---
+++ chagol.js
@@ -0,0 +1,8 @@
+function showChagol(){
+    document.getElementById('chagolerDiv').style.display='block';
+
+    document.getElementById('para').style.display='none';
+
+    document.getElementById('sound').play();
+}
+
